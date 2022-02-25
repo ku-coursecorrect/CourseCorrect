86cc5fef19063cb8957caa5ed8874569258905a6
dprebyl <drakeprebyl@gmail.com>
2022-02-25 16:13:40 -0600

    Plan save failed message bug fix

--- edit/save.php
+++ edit/save.php
@@ -2,9 +2,13 @@
     require_once "../common.php";
 	require_login();
 
-    // user_id included to ensure the plan belongs to the student
-    $result = $db->query("UPDATE plan SET plan_title = ?, json = ? WHERE plan_id = ? AND user_id = ?",
-        [$_POST["plan_title"], $_POST["json"], $_POST["plan_id"], $_SESSION["user_id"]]);
+    // Ensure the plan belongs to the user
+    // This is a separate query because an update which matches a row, but does not change any values, will return 0 rows affected
+    $plan = $db->query("SELECT plan_title FROM plan WHERE plan_id = ? AND user_id = ?",
+        [$_POST["plan_id"], $_SESSION["user_id"]]);
+    if (count($plan) != 1) crash(ErrorCode::PlanSaveFailed, $_POST);
 
-    if (!$result) crash(ErrorCode::PlanSaveFailed, $_POST);
+    // user_id included to ensure the plan belongs to the student
+    $db->query("UPDATE plan SET plan_title = ?, json = ? WHERE plan_id = ? AND user_id = ?",
+        [$_POST["plan_title"], $_POST["json"], $_POST["plan_id"], $_SESSION["user_id"]]);
 ?>
