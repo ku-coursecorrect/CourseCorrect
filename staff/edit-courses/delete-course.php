02062a62b9bec9db662090dbf9a211c5f5ac3194
Reece Mathews <mathewsreece@gmail.com>
2022-03-04 22:04:28 -0600

    Delete from requisite when course is deleted

--- staff/edit-courses/delete-course.php
+++ staff/edit-courses/delete-course.php
@@ -4,7 +4,10 @@
 
     if (array_key_exists('course_code', $_GET)) {
         $course_code = $_GET["course_code"];
-        $db->query("delete from course where course_code=?;", [$course_code]);
+        $course_id = $db->query("select course_id from course where course_code=?;", [$course_code])[0]["course_id"];
+        $db->query("delete from requisite where dependent_id=?;", [$course_id]);
+        $db->query("delete from requisite where course_id=?;", [$course_id]);
+        $db->query("delete from course where course_id=?;", [$course_id]);
     }
 
     // Redirect back to the courses page
