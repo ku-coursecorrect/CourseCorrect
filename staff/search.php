71091ca3f8815cba75ed3a0e2d277da48ab26044
James J. Krajicek <james.krajicek1@gmail.com>
2022-04-23 15:14:57 -0500

    Complete admin plan search. Implement search term lookup.

--- staff/search.php
+++ staff/search.php
@@ -25,29 +25,63 @@
                 <th scope="col">KUID #</th>
                 <th scope="col">Plan Name</th>
                 <th scope="col">Plan Status</th>
-                <th scope="col">View Plans</th> <!--May be able to comment out this line in the future-->
+                <th scope="col w-20"></th> <!--May be able to comment out this line in the future-->
             </thead>
             <tbody>
                 <?php
                     function parseAndCheckStuIds(string $ids) {
-                        $id_arr = explode(",",$ids);
+                        $arr = explode(",",$ids);
                         //TODO: Verify valid input (length and content).
-                        return ($id_arr);
+                        return ($arr);
                     }
-                    function parseAndCheckSearchTerms(string $search_tems) {
-                        $arr = explode(",",$search_tems);
+                    function parseAndCheckSearchTerms(string $search_terms) {
+                        $arr = explode(" ",$search_terms);
+                        foreach($arr as &$entry){
+                            $entry = '%' . $entry . '%';
+                        }
+                        unset($entry);
                         //TODO: Verify valid input (content).
-                        return ($term_arr);
+                        return ($arr);
                     }
                     if (!empty($_POST["stu_id_list"]) && !empty($_POST["search_term_list"])){
                         //TODO: Search for plans with a specific keyword in their title restricted to the student's whose IDs were passed in.
+                        $id_arr = parseAndCheckStuIds($_POST["stu_id_list"]);
+                        $term_arr = parseAndCheckSearchTerms($_POST["search_term_list"]);
+                        $to_print = $db->query("SELECT user.name, user.kuid, plan.plan_id, plan.plan_title, plan.plan_status 
+                                                FROM plan 
+                                                INNER JOIN user ON user.kuid IN (" . implode (',', array_fill(0, count($id_arr), '?')) .")
+                                                WHERE " . implode ("AND ", array_fill(0, count($term_arr), "plan.plan_title LIKE ? ")) . "
+                                                ORDER BY user.kuid DESC;",array_merge($id_arr,$term_arr));
+                        if (count($to_print)==0){
+                            echo '<tr><td colspan="5" class="text-center">No Entries Found.</td></tr>';
+                        }
+                        else{
+                            echo '<tr><td colspan="5" class="text-center font-weight-bold">Displaying plans containing the following terms: ' . implode(', ',explode(" ",$_POST["search_term_list"])) . '</td></tr>';
+                            foreach($to_print as $plan){
+                                echo '<tr data-plan_id ='. $plan["plan_id"].'>';
+                                echo '<td>' . $plan["name"] . '</td>';
+                                echo '<td>' . $plan["kuid"] . '</td>';
+                                echo '<td>' . $plan["plan_title"] . '</td>';
+                                if ($plan["plan_status"] == 4){
+                                    echo '<td><span class="badge badge-success">Complete</span></td>';
+                                }
+                                else
+                                {
+                                    echo '<td>' . planStatusToHTML($plan["plan_status"]) . '</td>';
+                                }
+                                echo '<td>';
+								echo '<a href="../edit?plan=' . $plan["plan_id"] . '" class="text-dark" title="Edit"><i class="fas fa-edit"></i></a>';
+                                echo '</td></tr>';
+                            }
+                        }
                     }
                     elseif (!empty($_POST["stu_id_list"])){
+                        //Search for plans owned by students whose IDs were passed in via "stu_id_list"
                         $id_arr = parseAndCheckStuIds($_POST["stu_id_list"]);
                         $to_print = $db->query("SELECT user.name, user.kuid, plan.plan_id, plan.plan_title, plan.plan_status 
                                                 FROM plan 
                                                 INNER JOIN user ON user.kuid IN (" . implode (',', array_fill(0, count($id_arr), '?')) .") 
-                                                ORDER BY user.kuid DESC",array_values($id_arr));
+                                                ORDER BY user.kuid DESC;",$id_arr);
                         if (count($to_print)==0){
                             echo '<tr><td colspan="5" class="text-center">No Entries Found.</td></tr>';
                         }
