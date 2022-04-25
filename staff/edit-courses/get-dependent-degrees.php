2c8dd9aeccaac04eea683089abce49218e2fed01
Reece Mathews <mathewsreece@gmail.com>
2022-04-24 22:30:12 -0500

    Only warn about course use in existing degrees

--- staff/edit-courses/get-dependent-degrees.php
+++ staff/edit-courses/get-dependent-degrees.php
@@ -4,7 +4,7 @@
     header('Content-type: application/json');
     $course_id = $_GET["course_id"];
     unset($_GET['course_id']);
-    $degrees = $db->query("SELECT major, year FROM degree_join_course as djc join degree as deg on djc.degree_id=deg.degree_id where course_id=?;", [$course_id]);
+    $degrees = $db->query("SELECT major, year FROM degree_join_course as djc join degree as deg on djc.degree_id=deg.degree_id where course_id=? AND deg.deleted_ts IS NULL;", [$course_id]);
     if (count($degrees) > 0) {
         echo json_encode($degrees);
     } else {
